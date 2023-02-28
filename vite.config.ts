9a1a310fb80f68b07f5b853ca1829fc197652d3a
Cherry <2858937488@qq.com>
2023-02-28 10:56:24 +0800

    style: vite.config.js简单配置完成

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@
  * @Author: Cherry 2858937488@qq.com
  * @Date: 2023-02-20 16:59:08
  * @LastEditors: Cherry 2858937488@qq.com
- * @LastEditTime: 2023-02-27 16:07:48
+ * @LastEditTime: 2023-02-28 10:55:55
  * @FilePath: \demo\vite.config.ts
  * @Description: 项目配置
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,19 @@ export default defineConfig({
     alias: [
       {
         find: /@\//,
-        replacement: `${path.resolve(process.cwd(), 'src')}/`
+        replacement: `${resolve(process.cwd(), 'src')}/`
       }
     ]
+  },
+
+  server: {
+    host: true,
+    port: 8008,
+    origin: 'http://localhost:8008',
+    open: './index.html'
+  },
+
+  esbuild: {
+    pure: process.env.NODE_ENV === 'production' ? ['console.log', 'debugger'] : []
   }
 })
