8e23506624650cdd5647e2388c3c36dadcaaecc4
Cherry <2858937488@qq.com>
2023-03-01 18:34:14 +0800

    feat: 新增UI组件插件配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,28 @@
  * @Author: Cherry 2858937488@qq.com
  * @Date: 2023-02-20 16:59:08
  * @LastEditors: Cherry 2858937488@qq.com
- * @LastEditTime: 2023-02-28 10:55:55
+ * @LastEditTime: 2023-03-01 15:29:23
  * @FilePath: \demo\vite.config.ts
  * @Description: 项目配置
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          resolveIcons: true
+        })
+      ]
+    })
+  ],
 
   resolve: {
     alias: [
